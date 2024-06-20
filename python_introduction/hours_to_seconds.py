302514b1bfbcee483c8c0ec1e4da02bcf88e062f
mohamedkamelaly <mohamedkamelali93@gmail.com>
2024-06-20 18:11:02 +0300

    fix seconds equal hour * 3600

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,5 +1,5 @@
 #Convert Hours to Seconds
 hours = 2
-seconds = hours * 60 * 60 
+seconds = hours * 3600
 # 2 hour(s) is 7200 seconds.
 print ( hours, "hour(s) is",seconds ,".")
--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
 # Objective: Apply basic Python arithmetic operations and variable assignments to 
 # calculate the simple interest on a given principal amount, rate of interest, and time.
-
+# I = P * R * T
 principal = 1000
 rate = 0.05
 time = 3
