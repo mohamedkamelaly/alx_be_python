2b2b454f506ce7181fe6d975d7d027cdaf6b43a5
mohamedkamelaly <mohamedkamelali93@gmail.com>
2024-06-20 17:58:48 +0300

    python script to calculte simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+# Objective: Apply basic Python arithmetic operations and variable assignments to 
+# calculate the simple interest on a given principal amount, rate of interest, and time.
+
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+print ("The simple interest is:  " + str(interest))
