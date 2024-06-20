f34ee48f22c767ccaddc717b17d785b9986a3e2c
mohamedkamelaly <mohamedkamelali93@gmail.com>
2024-06-20 17:50:25 +0300

    it include introduction to python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+# basic arithmetic operations in Python by performing predefined calculations.
+number1 = 10
+number2 = 5
+Addition = int(number1) + int(number2)
+Subtraction = int(number1) - int(number2)
+Multiplication = int(number1) * int(number2)
+#Addition of 10 and 5 is 15
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(Addition))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(Subtraction))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(Multiplication))
