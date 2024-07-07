3376e8b972724939ac37eed86fcad2c22fb384c9
mohamedkamelaly <mohamedkamelali93@gmail.com>
2024-07-07 23:31:40 +0300

    update on temp_conversion tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -9,7 +9,7 @@ def convert_to_celsius(fahrenheit):
 
 
 def convert_to_fahrenheit(celsius):
-    return (celsius - 32) * CELSIUS_TO_FAHRENHEIT_FACTOR
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 if temperature_type == "C":
     #celsius = temperature
