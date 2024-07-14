ff2eaf2e41021b9b63bb271a69e66980aba9c474
mohamedkamelaly <mohamedkamelali93@gmail.com>
2024-07-15 00:28:58 +0300

    Testing your Code modify label

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -14,12 +14,12 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(-1, 3), -4)
         self.assertEqual(self.calc.subtract(3, 3), 0)
         # Add more assertions to thoroughly test the add method.
-    def test_multiply(self):
+    def test_multiplication(self):
         """Test the Multiply method."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
         # Add more assertions to thoroughly test the add method.
-    def test_divide(self):
+    def test_division(self):
         """Test the Division method."""
         self.assertEqual(self.calc.divide(9, 3), 3)
         self.assertEqual(self.calc.divide(-1, 0), None)
